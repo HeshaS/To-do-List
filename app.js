296fe1613779423edf70992741ee32264415c82c
HeshaS <37592741+HeshaS@users.noreply.github.com>
2022-01-21 15:25:20 -0500

    Adding content in js file

---
+++ app.js
@@ -0,0 +1,19 @@
+// Selectors
+
+const todoInput=document.querySelector('.todo-input');
+const todoButton=document.querySelector('.todo-button');
+const todoList=document.querySelector('.todo-list');
+
+//Event Listeners
+
+todoButton.addEventListener('click',addTodo);
+
+
+
+//Functions
+
+function addTodo(Event){
+
+
+    console.log('Hello');
+}
